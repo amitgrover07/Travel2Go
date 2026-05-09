023dd624719e3b4c54d368001f2cf9b9c9ad694b
amit grover <grover.amit443@gmail.com>
2026-05-09 13:10:28 +0530

    change

--- microservices/package-service/src/main/java/com/travel2go/backend/config/FirestoreConfig.java
+++ microservices/package-service/src/main/java/com/travel2go/backend/config/FirestoreConfig.java
@@ -1,6 +1,6 @@
 package com.travel2go.backend.config;
 
-import com.google.cloud.spring.data.firestore.FirestoreOptionsCustomizer;
+import com.google.cloud.firestore.FirestoreOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +8,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FirestoreConfig {
 
+    @Value("${spring.cloud.gcp.firestore.project-id:travel2go-495007}")
+    private String projectId;
+
     @Value("${spring.cloud.gcp.firestore.database-id:travel2go-db}")
     private String databaseId;
 
     @Bean
-    public FirestoreOptionsCustomizer firestoreOptionsCustomizer() {
-        return builder -> {
-            builder.setDatabaseId(databaseId);
-        };
+    public FirestoreOptions firestoreOptions() {
+        return FirestoreOptions.newBuilder()
+                .setProjectId(projectId)
+                .setDatabaseId(databaseId)
+                .build();
     }
 }
