54c3e9d31dd1875adff711d1407e48ee50f2dea5
amit grover <grover.amit443@gmail.com>
2026-05-10 10:27:43 +0530

    text issue

--- frontend/src/utils/formatUtils.js
+++ frontend/src/utils/formatUtils.js
@@ -55,6 +55,9 @@ export const cleanHtmlForDisplay = (html) => {
   if (!html) return '';
   let clean = html;
   
+  // Replace non-breaking spaces with standard spaces to allow natural word wrapping
+  clean = clean.replace(/&nbsp;/g, ' ').replace(/\u00A0/g, ' ');
+  
   // 1. Merge words split by \n or <br> mid-word (lowercase/hyphen followed by break followed by lowercase)
   clean = clean.replace(/([a-z-])\s*(?:<br\s*\/?>|\n)\s*([a-z])/g, '$1$2');
   
