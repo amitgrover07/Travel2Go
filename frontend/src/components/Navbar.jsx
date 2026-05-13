5c0f7990a0471e894ba9e23cc7127bf073b423c2
amit grover <grover.amit443@gmail.com>
2026-05-13 16:57:14 +0530

    logo changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 flex items-center">
             <Link to="/" className="flex items-center">
-              <img src="/logo.png" alt="travel2go.in" className="h-10 w-auto object-contain flex-shrink-0" />
+              <img src="/logo.svg" alt="Travel2Go.in" className="h-12 w-auto object-contain flex-shrink-0" />
             </Link>
           </div>
           <div className="flex items-center space-x-4">
