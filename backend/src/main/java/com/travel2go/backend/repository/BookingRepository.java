6df7ceae5fb063ec29b0fffa497b209e93aa0381
Amit Grover <amitgrover1992@live.com>
2026-07-06 10:36:18 +0530

    chore: retire backend/ monolith, superseded by microservices/ split

--- backend/src/main/java/com/travel2go/backend/repository/BookingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.travel2go.backend.repository;
-
-import com.travel2go.backend.model.Booking;
-import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends FirestoreReactiveRepository<Booking> {
-}
--- backend/src/main/java/com/travel2go/backend/repository/GlobalSettingsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.travel2go.backend.repository;
-
-import com.google.cloud.spring.data.firestore.FirestoreReactiveRepository;
-import com.travel2go.backend.model.GlobalSettings;
-
-public interface GlobalSettingsRepository extends FirestoreReactiveRepository<GlobalSettings> {
-}
